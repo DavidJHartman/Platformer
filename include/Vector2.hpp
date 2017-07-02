b9c957bfac1e24942543ca624ed5f8d4537eff17
David Hartman <djhartman1216@gmail.com>
2017-07-01 18:30:02 -0700

    Added in Vector2 functionality, next step is to add Vector math

---
+++ include/Vector2.hpp
@@ -0,0 +1,71 @@
+#ifndef VECTOR2_HPP_INCLUDED
+#define VECTOR2_HPP_INCLUDED
+
+
+enum VectorType {
+    Polar,
+    Cartesian
+};
+
+template <class T>
+class Vector2 {
+
+public:
+    T x, y;
+    Vector2( T x, T y ) { this->x = x; this->y = y; }
+    Vector2( ) { x = 0; y = 0; }
+    ~Vector2( ){ }
+
+    //////////////////////
+    // MEMBER FUNCTIONS //
+    //////////////////////
+
+    void PrintData ( VectorType type ) {
+        if ( type == Polar ) {
+            std::cout << "Polar Vector Data: \n" << "magnitude: \t" << sqrt( x * x + y * y ) << "\nAngle: \t" << atan( y / x ) * ( 180 / 3.14159 ) << std::endl;
+        }
+        if ( type == Cartesian ) {
+            std::cout << "Cartesian Vector Data: \nX: " << x << "\tY: " << y << std::endl << std::endl;
+        }
+    }
+
+    //////////////////////////
+    // OVERLOADED OPERATORS //
+    //////////////////////////
+
+    // Addition operators
+    Vector2<T> operator + ( Vector2& V ) {
+        return Vector2( x + V.x, y + V.y );
+    }
+    void operator += ( Vector2& V ) {
+        x += x; y += y;
+    }
+
+    // Subtraction operators
+    Vector2<T> operator - ( Vector2& V ) {
+        return Vector2( x - V.x, y - V.y );
+    }
+    void operator -= ( Vector2& V ) {
+        x -= x; y -= y;
+    }
+
+    // Scalar operators
+    Vector2<T> operator * ( T Scalar ) {
+        return Vector2( x * Scalar, y * Scalar );
+    }
+    Vector2<T> operator / ( T Scalar ) {
+        return Vector2( x / Scalar, y / Scalar );
+    }
+    void operator *= ( T Scalar ) {
+        x *= Scalar; y *= Scalar;
+    }
+    void operator /= ( T Scalar ) {
+        x /= Scalar; y /= Scalar;
+    }
+};
+
+typedef Vector2<int> Vector2i;
+typedef Vector2<float> Vector2f;
+typedef Vector2<double> Vector2d;
+
+#endif // VECTOR2_HPP_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+#include <Vector2.hpp>
 
-int main(int argc, char ** argv) {
-  std::cout << "Hello, world!";
+using namespace std;
+
+int main()
+{
+
+    sf::RenderWindow window( sf::VideoMode( 800, 600 ), "Sneak" );
+    sf::Event event;
+
+    Vector2f laughin(1.0f, 1.0f);
+    Vector2f At(1, 1);
+
+    Vector2f You = laughin + At;
+
+    You.PrintData( Cartesian );
+
+    while ( window.isOpen() ) {
+        while ( window.pollEvent( event ) ) {
+            if ( event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+
+        window.display();
+    }
 }
